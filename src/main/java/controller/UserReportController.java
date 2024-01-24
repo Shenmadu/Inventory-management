0559df25d829311886fd70d1e028aab7172c9e44
shenmadu <tashen.madu@gmail.com>
2024-01-24 14:58:57 +0530

    Jasper report use

--- src/main/java/controller/UserReportController.java
+++ src/main/java/controller/UserReportController.java
@@ -1,12 +1,18 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import db.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class UserReportController {
 
@@ -30,6 +36,21 @@ public class UserReportController {
     }
 
     public void allCustomerBtnOnAction(ActionEvent actionEvent) {
+        try {
+            JasperDesign design = JRXmlLoader.load("src/main/resources/reports/Customer.jrxml");
+
+            JasperReport jasperReport = JasperCompileManager.compileReport(design);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, DBConnection.getInstance().getConnection());
+            JasperViewer.viewReport(jasperPrint,false);
+
+
+        } catch (JRException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void searchCustomerBtnOnAction(ActionEvent actionEvent) {
